8ab19c8ed10cfbbbfde2f21de0e8bf1070b6bb58
Gabriel Simoes <gmachadosimoes@gmail.com>
2021-12-15 23:42:53 +0000

    fixed loop, game works as expected

--- guessing-game.cpp
+++ guessing-game.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
-#include<random>
+#include<cstdlib>
+#include<ctime>
+
 using namespace std;
 
-int main() {
+int main() 
+{
     cout << endl;
     cout << "****************************" << endl;
     cout << endl;
@@ -10,24 +13,45 @@ int main() {
     cout << endl;
     cout << "****************************" << endl;
     cout << endl;
+    cout << "Choose the difficulty level:" << endl;
+    cout << "Easy (E), Intermediate (M) or Hard (H)" << endl;
 
-    int secretNumber = rand() %100;
+    char difficultyLevel;
+    cin >> difficultyLevel;
+
+    int numberOfAttempts;
+
+    if (difficultyLevel == 'E')
+    {
+        numberOfAttempts = 15;
+    }
+    else if (difficultyLevel == 'I')
+    {
+        numberOfAttempts = 10;
+    }
+    else {
+        numberOfAttempts = 5;
+    }
+     
+
+    srand(time(NULL));
+    const int SECRET_NUMBER = rand() % 100;
     bool wrongGuess = true;
     int attempts = 0;
     double score = 1000.0;
 
-    while (wrongGuess)
-    {
-        attempts++;
+    for (attempts = 1; attempts <= numberOfAttempts; attempts++) {
+        
         int guess;
         cout << "Attempt number " << attempts << endl;
         cout << "What is your guess?" << endl;
         cin >> guess;
 
-        double lost_score = abs(guess - secretNumber) / 2.0;
-        score -= lost_score;
-        bool correctGuess = guess == secretNumber;
-        bool above = guess > secretNumber;
+        double lostScore = abs(guess - SECRET_NUMBER)/2.0;
+        score -= lostScore;
+
+        bool correctGuess = guess == SECRET_NUMBER;
+        bool above = guess > SECRET_NUMBER;
         
         cout << "Your guess is: " << guess;
         cout << endl;
@@ -36,18 +60,28 @@ int main() {
         {
             cout << "Spot on! This is the secret number." << endl;
             wrongGuess = false;
+            break;
         }
-        else if (above) {
-            cout << "Your guess is above the secrete number, try again!" << endl;
+        else if (above) 
+        {
+            cout << "Your guess is above the secret number." << endl;
         }
-        else {
-            cout << "Your guess is below the secret number, try again" << endl;
+        else 
+        {
+            cout << "Your guess is below the secret number." << endl;
         }
-    
     }
+
        cout << "Game over!" << endl;
-       cout << "You nailed it in " << attempts << " attempts." << endl;
-       cout.precision(2);
-       cout << fixed;
-       cout << "Your score is " << score << " pontos." << endl;
+
+       if(wrongGuess) {
+           cout << "You lose, try again!" << endl;
+       }
+       else {
+            cout << "You nailed it in " << attempts << " attempts." << endl;
+            cout.precision(2);
+            cout << fixed;
+            cout << "Your score is " << score << " pontos." << endl;
+
+       }
 }
